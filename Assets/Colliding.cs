b0338d007bd6f1dc174987a7085124d01d59d6b6
yuvaln <yuvalno@gmail.com>
2018-03-19 22:52:37 +0200

    Final Enum rotation

---
+++ Assets/Colliding.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colliding : MonoBehaviour
+{
+    public int Green_floor = 1;
+    public enum Props_Enum { Player_BLue, Player_BLack, Player_Yellow, Player_Red, Player_Green };
+    public Props_Enum Current_floor = Props_Enum.Player_Green;
+    private Props_Enum Current_props;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+      
+      if (Current_floor != Current_props)
+        {
+            Debug.Log("I Clide");
+        }
+
+    }
+    public void SetColorToBlue()
+    {
+        
+        Current_props = Props_Enum.Player_BLue;
+    }
+
+    public void SetColorToGreen()
+    {
+        
+        Current_props = Props_Enum.Player_Green;
+    }
+
+    public void SetColorToBlack()
+    {
+        
+        Current_props = Props_Enum.Player_BLack;
+    }
+
+    public void SetColorToYellow()
+    {
+        
+        Current_props = Props_Enum.Player_Yellow;
+    }
+
+    public void SetColorToRed()
+    {
+        
+        Current_props = Props_Enum.Player_Red;
+    }
+}
--- Assets/Player/Movment.cs
+++ Assets/Player/Movment.cs
@@ -10,14 +10,21 @@ public class Movment : MonoBehaviour {
     public bool useSmoothRide;
     public Renderer rend;
     public Color GreenColor = new Color();
+    //Define Enum
+    public enum Props_Enum { Player_BLue, Player_BLack, Player_Yellow, Player_Red, Player_Green };
     
+    //This what you need to show in the inspector.
+    public Props_Enum Current_props;
+    public string Current_Floor;
+
 
 
 
     // Use this for initialization
     void Start () {
         rend = GetComponent<Renderer>();
-        
+        rend.material.color = Color.green;
+        Current_Floor = "GreenFloor";
         //rend.material.color = altColor;
 
     }
@@ -26,7 +33,7 @@ public class Movment : MonoBehaviour {
     void Update()
     {
 
-
+        Hit_object();
         transform.Translate(Vector3.forward * Time.deltaTime * speedPlayer);
 
         //controlling the player left and right
@@ -60,70 +67,81 @@ public class Movment : MonoBehaviour {
             }
         }
 
-        
+        Hit_object();
     }
 
 
 
     public void OnTriggerEnter(Collider other)
     {
-        
-        if (other.tag == "GreenFloor")
-        {
-            if (rend.material.color != other.gameObject.GetComponent<Renderer>().material.color)
-            {
-                Debug.Log("I collide with something ");
-            }
-        }
-        if (other.tag == "BlueFloor")
-        {
-            if (rend.material.color != other.gameObject.GetComponent<Renderer>().material.color)
-            {
-                Debug.Log("I collide with something ");
-            }
-        }
-        if (other.tag == "RedFloor")
-        {
-            if (rend.material.color != other.gameObject.GetComponent<Renderer>().material.color)
-            {
-                Debug.Log("I collide with something ");
-            }
-        }
+
+        Current_Floor = other.gameObject.tag;
+        Hit_object();
+
+
     }
 
 
 
+    public void Hit_object()
+    {
+        if (Current_Floor == "RedFloor")
+        {
+            if (Current_props != Props_Enum.Player_Red)
+            {
+                Debug.Log("I colide something else22222");
+            }
+        }
+        if (Current_Floor == "BlueFloor")
+        {
+            if (Current_props != Props_Enum.Player_BLue)
+            {
+                Debug.Log("I colide something else1111");
+            }
+        }
+        if (Current_Floor == "GreenFloor")
+        {
+            if (Current_props != Props_Enum.Player_Green)
 
+            {
+
+                Debug.Log("I colide something else33333");
+
+            }
+        }
+    }
 
     public void SetColorToBlue()
     {
         rend.material.color = Color.blue;
+        Current_props = Props_Enum.Player_BLue;
     }
 
-    public void SetColorToGreen(Collider other)
+    public void SetColorToGreen()
     {
-        rend.material.color = other.gameObject.GetComponent<Renderer>().material.color;
+        rend.material.color = Color.green;
+        Current_props = Props_Enum.Player_Green;
     }
 
     public void SetColorToBlack()
     {
         rend.material.color = Color.black;
+        Current_props = Props_Enum.Player_BLack;
     }
 
     public void SetColorToYellow()
     {
         rend.material.color = Color.yellow;
+        Current_props = Props_Enum.Player_Yellow;
     }
 
     public void SetColorToRed()
     {
         rend.material.color = Color.red;
+        Current_props = Props_Enum.Player_Red;
     }
 
-    public void SetColorToGreenq()
-    {
-        rend.material.color = Color.green;
-    }
+    
 
 
 
